0593a3eedecaa2755d2d425f992b83a27759f8e6
oooslhustji-del <oooslhustji@gmail.com>
2026-02-15 15:51:34 +0800

    update name

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,11 +3,11 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'HUSTJI'
+export const headline = 'student'
 export const introduction =
   "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
-export const email = 'tong.hu@scls-sh.org'
+export const email = 'secret'
 export const githubUsername = 'derekhut'
 
 // about page
